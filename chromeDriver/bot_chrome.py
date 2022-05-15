8afe72c4a1d8489ee1c33e0798d7dfa36cd54cf7
Rybak Dmytro <codingclaud@gmail.com>
2022-05-15 19:48:43 +0300

    Improved code readability

--- chromeDriver/bot_chrome.py
+++ chromeDriver/bot_chrome.py
@@ -4,21 +4,24 @@ from typing import List
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-from chromeDriver.auth_data import email, password
+# from chromeDriver._auth_data import _EMAIL, _PASSWORD
+from chromeDriver import _auth_data
 from params import URL, DRIVER_PATH, amount_connects
 
 
-URL_PARSE = f"https://www.linkedin.com/search/results/people/?keywords=it%20recruiter%2F%20it%20acquisition%20specialist&origin=CLUSTER_EXPANSION&page=1&sid=zZd"
+URL_PARSE = f"https://www.linkedin.com/search/results/people/?" \
+            f"keywords=it%20recruiter%2F%20it%20acquisition%20" \
+            f"specialist&origin=CLUSTER_EXPANSION&page=1&sid=zZd"
 
 
 def register(driver) -> None:
     email_input = driver.find_element(by=By.ID, value="username")
     email_input.clear()
-    email_input.send_keys(email)
+    email_input.send_keys(_auth_data.EMAIL)
 
     password_input = driver.find_element(by=By.ID, value="password")
     password_input.clear()
-    password_input.send_keys(password)
+    password_input.send_keys(_auth_data.PASSWORD)
 
     login_button = driver.find_element(
         by=By.CLASS_NAME,
@@ -49,7 +52,8 @@ def run(driver) -> None:
 
         while connected != amount_connects:
             driver.get(url=f"https://www.linkedin.com/search/results/people/?"
-                           f"keywords=it%20recruiter%2F%20it%20acquisition%20specialist&origin=CLUSTER_EXPANSION&page={page}&sid=zZd")
+                           f"keywords=it%20recruiter%2F%20it%20acquisition%20"
+                           f"specialist&origin=CLUSTER_EXPANSION&page={page}&sid=zZd")
 
             all_buttons = driver.find_elements(by=By.TAG_NAME, value="button")
             connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]

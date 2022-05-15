280201d7388ed51ddd13730002a9ebf52b468482
Rybak Dmytro <codingclaud@gmail.com>
2022-05-15 23:46:24 +0300

    v2.0

--- chromeDriver/bot_chrome.py
+++ chromeDriver/bot_chrome.py
@@ -5,70 +5,86 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from chromeDriver import _auth_data
-from params import URL, DRIVER_PATH, amount_connects
+from params import Params
 
 
-URL_PARSE = f"https://www.linkedin.com/search/results/people/?" \
-            f"keywords=it%20recruiter%2F%20it%20acquisition%20" \
-            f"specialist&origin=CLUSTER_EXPANSION&page=1&sid=zZd"
+class LinkedinBot:
 
+    def __init__(self, driver):
+        self.__driver = driver
 
-def register(driver) -> None:
-    email_input = driver.find_element(by=By.ID, value="username")
-    email_input.clear()
-    email_input.send_keys(_auth_data.EMAIL)
+    def __register(self) -> None:
+        # Initializing account data
+        __account_data = _auth_data.AccountData()
 
-    password_input = driver.find_element(by=By.ID, value="password")
-    password_input.clear()
-    password_input.send_keys(_auth_data.PASSWORD)
+        # Connecting to account
+        email_input = self.__driver.find_element(by=By.ID, value="username")
+        email_input.clear()
+        email_input.send_keys(__account_data.EMAIL)
 
-    login_button = driver.find_element(
-        by=By.CLASS_NAME,
-        value="from__button--floating"
-    )
-    login_button.click()
+        password_input = self.__driver.find_element(by=By.ID, value="password")
+        password_input.clear()
+        password_input.send_keys(__account_data.PASSWORD)
 
+        login_button = self.__driver.find_element(
+            by=By.CLASS_NAME,
+            value="from__button--floating"
+        )
+        login_button.click()
 
-def connect_to_hr(connect_buttons: List, driver, connected) -> int:
-    for btn in connect_buttons:
-        driver.execute_script("arguments[0].click();", btn)
-        time.sleep(1)
+    def __connect_to_hr(self, connect_buttons: List, connected: int) -> int or dict:
+        for btn in connect_buttons:
+            self.__driver.execute_script("arguments[0].click();", btn)
+            time.sleep(0.6)
 
-        send = driver.find_element(by=By.XPATH, value="//button[@aria-label='Send now']")
-        driver.execute_script("arguments[0].click();", send)
-        connected += 1
-        time.sleep(1)
-    return connected
+            send = self.__driver.find_element(by=By.XPATH, value="//button[@aria-label='Send now']")
+            self.__driver.execute_script("arguments[0].click();", send)
+            time.sleep(0.6)
 
+            try:
+                # If we got an alert - stopping the program
+                if self.__driver.find_element(by=By.ID, value="ip-fuse-limit-alert__header"):
+                    return {"connected": connected, "stop_function": True}
+            except Exception as ex:
+                print(ex)
 
-def run(driver) -> None:
-    try:
-        driver.get(url=URL)
-        register(driver)
+            connected += 1
+        return connected
 
-        connected = 0
-        page = 1
+    def run(self) -> None:
+        try:
+            __parameters = Params()
+            self.__driver.get(url=__parameters.URL)
+            self.__register()
 
-        while connected != amount_connects:
-            driver.get(url=f"https://www.linkedin.com/search/results/people/?"
-                           f"keywords=it%20recruiter%2F%20it%20acquisition%20"
-                           f"specialist&origin=CLUSTER_EXPANSION&page={page}&sid=zZd")
+            _connected = 0
+            _page = 60
 
-            all_buttons = driver.find_elements(by=By.TAG_NAME, value="button")
-            connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+            while _connected != __parameters.amount_connects:
+                self.__driver.get(url=f"https://www.linkedin.com/search/results/people/?"
+                                      f"keywords=it%20recruiter%2F%20it%20acquisition%20"
+                                      f"specialist&origin=CLUSTER_EXPANSION&page={_page}&sid=zZd")
 
-            connected = connect_to_hr(connect_buttons=connect_buttons, driver=driver, connected=connected)
+                __all_buttons = self.__driver.find_elements(by=By.TAG_NAME, value="button")
+                __connect_buttons = [btn for btn in __all_buttons if btn.text == "Connect"]
 
-            page += 1
-        print(f"{connected} HR's connected")
-    except Exception as ex:
-        print(ex)
-    finally:
-        driver.close()
-        driver.quit()
+                _connected = self.__connect_to_hr(connect_buttons=__connect_buttons, connected=_connected)
+
+                if type(_connected) is dict:
+                    print(f"{_connected['connected']} HR's connected")
+                    break
+
+                _page += 1
+        except Exception as ex:
+            print(ex)
+        finally:
+            self.__driver.close()
+            self.__driver.quit()
 
 
 if __name__ == "__main__":
-    chrome_driver = webdriver.Chrome(DRIVER_PATH)
+    __parameters = Params()
+    chrome_driver = webdriver.Chrome(__parameters.DRIVER_PATH)
+    bot = LinkedinBot(chrome_driver)
 
-    run(driver=chrome_driver)
+    bot.run()
--- chromeDriver/params.py
+++ chromeDriver/params.py
@@ -1,6 +1,8 @@
-URL = "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2F" \
-      "www%2Elinkedin%2Ecom%2Fsearch%2Fresults%2Fpeople%2F%3Fkeywords%3Dit%2520" \
-      "recruiter%252F%2520it%2520acquisition%2520specialist%26origin%" \
-      "3DCLUSTER_EXPANSION%26sid%3DK3x&fromSignIn=true&trk=cold_join_sign_in"
-DRIVER_PATH = r"E:\Mate Academy\Pet projects\Parsers\LinkedinHRs\chromeDriver\chromedriver.exe"
-amount_connects = 80
+class Params:
+    def __init__(self,):
+        self.URL = "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2F" \
+                  "www%2Elinkedin%2Ecom%2Fsearch%2Fresults%2Fpeople%2F%3Fkeywords%3Dit%2520" \
+                  "recruiter%252F%2520it%2520acquisition%2520specialist%26origin%" \
+                  "3DCLUSTER_EXPANSION%26sid%3DK3x&fromSignIn=true&trk=cold_join_sign_in"
+        self.DRIVER_PATH = r"E:\Mate Academy\Pet projects\Parsers\LinkedinHRs\chromeDriver\chromedriver.exe"
+        self.amount_connects = 80
